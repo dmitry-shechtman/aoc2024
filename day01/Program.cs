4fffd5d32db2323632a80cc40af651f43638ace7
Dmitry Shechtman <damnian@gmail.com>
2024-12-01 15:57:04 +0200

    Add solution Add day01

---
+++ day01/Program.cs
@@ -0,0 +1,11 @@
+﻿using aoc;
+
+var input = File.ReadAllLines("input.txt").Select(s => s.Split("   "));
+var a = input.Select(s => int.Parse(s[0])).Order().ToArray();
+var b = input.Select(s => int.Parse(s[1])).Order().ToArray();
+
+var d = a.Zip(b).Sum(t => Math.Abs(t.First - t.Second));
+Console.WriteLine(d);
+
+var s = a.Sum(x => x * b.Count(y => x == y));
+Console.WriteLine(s);
