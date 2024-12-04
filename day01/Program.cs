e5d969a24d105a5ed41b1b099d78bae67a366069
Dmitry Shechtman <damnian@gmail.com>
2024-12-04 02:50:45 +0200

    Simplify day01 part 1

--- day01/Program.cs
+++ day01/Program.cs
@@ -4,7 +4,7 @@ var input = File.ReadAllLines("input.txt").Select(s => s.Split("   "));
 var a = input.Select(s => int.Parse(s[0])).Order().ToArray();
 var b = input.Select(s => int.Parse(s[1])).Order().ToArray();
 
-var d = a.Zip(b).Sum(t => Math.Abs(t.First - t.Second));
+var d = a.Zip(b, (x, y) => Math.Abs(x - y)).Sum();
 Console.WriteLine(d);
 
 var s = a.Sum(x => x * b.Count(y => x == y));
