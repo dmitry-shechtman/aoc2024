6f74ce79228b83f41e3a450e776423db46d47ba8
Dmitry Shechtman <damnian@gmail.com>
2024-12-05 04:46:19 +0200

    Inline initializations

--- day04/Program.cs
+++ day04/Program.cs
@@ -4,20 +4,16 @@ using aoc.Grids;
 var s = File.ReadAllText("input.txt").Trim();
 var m = MultiGrid.Parse(s, "SMXA");
 
-Console.WriteLine(Solve(Init1()));
-Console.WriteLine(Solve(Init2()));
+Console.WriteLine(Solve(MooreGrid.Headings
+    .Select(h =>
+        new[] { new[] { -h, h, h * 2 } })));
 
-Vector[][][] Init1() => MooreGrid.Headings
-    .Select(h => new[] { new[] { -h, h, h * 2 } })
-    .ToArray();
-
-Vector[][][] Init2() => DiagGrid.Headings
+Console.WriteLine(Solve(DiagGrid.Headings
     .SelectMany((g, i) =>
         DiagGrid.Headings[..i].Select(h =>
-            new[] { new[] { g, -g }, new[] { h, -h }}))
-    .ToArray();
+            new[] { new[] { g, -g }, new[] { h, -h } }))));
 
-int Solve(Vector[][][] hhhh) => m[3].Sum(p =>
+int Solve(IEnumerable<Vector[][]> hhhh) => m[3].Sum(p =>
     hhhh.Count(hhh =>
         hhh.All(hh =>
             hh.Zip(m, (h, g) =>
