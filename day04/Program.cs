0ae81212f53d2a92ccd6d8f9d9472db86bab93d6
Dmitry Shechtman <damnian@gmail.com>
2024-12-05 04:36:20 +0200

    Move A to end

--- day04/Program.cs
+++ day04/Program.cs
@@ -2,22 +2,22 @@
 using aoc.Grids;
 
 var s = File.ReadAllText("input.txt").Trim();
-var m = MultiGrid.Parse(s, "SAMX");
+var m = MultiGrid.Parse(s, "SMXA");
 
 Console.WriteLine(Solve(Init1()));
 Console.WriteLine(Solve(Init2()));
 
 Vector[][][] Init1() => MooreGrid.Headings
-    .Select(h => new[] { new[] { -h, default, h, h * 2 } })
+    .Select(h => new[] { new[] { -h, h, h * 2 } })
     .ToArray();
 
 Vector[][][] Init2() => DiagGrid.Headings
     .SelectMany((g, i) =>
         DiagGrid.Headings[..i].Select(h =>
-            new[] { new[] { g, Vector.Zero, -g }, new[] { h, Vector.Zero, -h }}))
+            new[] { new[] { g, -g }, new[] { h, -h }}))
     .ToArray();
 
-int Solve(Vector[][][] hhhh) => m[1].Sum(p =>
+int Solve(Vector[][][] hhhh) => m[3].Sum(p =>
     hhhh.Count(hhh =>
         hhh.All(hh =>
             hh.Zip(m, (h, g) =>
