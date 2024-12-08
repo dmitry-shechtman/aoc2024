d71cde1efe6fc2be0a98aa876442f342412db973
Dmitry Shechtman <damnian@gmail.com>
2024-12-08 05:26:13 +0200

    Flatten tuples

--- day07/Program.cs
+++ day07/Program.cs
@@ -4,32 +4,32 @@
 Console.WriteLine(Solve(1));
 Console.WriteLine(Solve(2));
 
-(long z, long[] v) ParseOne(string t)
+long[] ParseOne(string t)
 {
-    var z = 0L;
+    var l = 0L;
     var j = 0;
     for (; t[j] != ':'; j++)
-        z = z * 10 + t[j] - '0';
-    var v = new long[t.Count(c => c == ' ')];
-    var l = 0L;
-    for ((var i, j) = (0, j + 2); j < t.Length; ++j)
+        l = l * 10 + t[j] - '0';
+    var v = new long[t.Count(c => c == ' ') + 1];
+    v[0] = l;
+    for ((var i, j, l) = (1, j + 2, 0); j < t.Length; ++j)
         if (t[j] == ' ')
             (v[i++], l) = (l, 0L);
         else
             l = l * 10 + t[j] - '0';
     v[^1] = l;
-    return (z, v);
+    return v;
 }
 
 long Solve(int p) =>
-    vv.AsParallel().Sum(t => IsMatch(t.v[0], t, 1, p + 1) ? t.z : 0);
+    vv.AsParallel().Sum(t => IsMatch(t[1], t, 2, p + 1) ? t[0] : 0);
 
-bool IsMatch(long a, (long z, long[] v) t, int k, int n) =>
-    k < t.v.Length
-        ? t.z >= a && IsMatch2(a, t, k, n)
-        : t.z == a;
+bool IsMatch(long a, long[] t, int k, int n) =>
+    k < t.Length
+        ? t[0] >= a && IsMatch2(a, t, k, n)
+        : t[0] == a;
 
-bool IsMatch2(long a, (long z, long[] v) t, int k, int n)
+bool IsMatch2(long a, long[] t, int k, int n)
 {
     for (int i = 0; i < n; i++)
         if (IsMatch(Calc(a, t, k, i), t, k + 1, n))
@@ -37,11 +37,11 @@ bool IsMatch2(long a, (long z, long[] v) t, int k, int n)
     return false;
 }
 
-long Calc(long a, (long z, long[] v) t, int k, int i) => i switch
+long Calc(long a, long[] t, int k, int i) => i switch
 {
-    0 => a + t.v[k],
-    1 => a * t.v[k],
-    2 => Concat(a, t.v[k]),
+    0 => a + t[k],
+    1 => a * t[k],
+    2 => Concat(a, t[k]),
     _ => throw new NotImplementedException()
 };
 
