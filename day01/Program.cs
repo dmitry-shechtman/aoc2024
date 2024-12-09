5337bba5e5d6bb25b2b9c0bf50118c5c65d3a838
Dmitry Shechtman <damnian@gmail.com>
2024-12-09 21:44:57 +0200

    Upgrade to net7.0

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,6 +1,4 @@
-﻿using aoc;
-
-var input = File.ReadAllLines("input.txt").Select(s => s.Split("   "));
+﻿var input = File.ReadAllLines("input.txt").Select(s => s.Split("   "));
 var a = input.Select(s => int.Parse(s[0])).Order().ToArray();
 var b = input.Select(s => int.Parse(s[1])).Order().ToArray();
 
