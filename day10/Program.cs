1e16cc7f02b677042b5487bc5bbea1e4eb4001fe
Dmitry Shechtman <damnian@gmail.com>
2024-12-10 08:44:09 +0200

    Use Grid as accumulator

--- day10/Program.cs
+++ day10/Program.cs
@@ -8,10 +8,9 @@ Console.WriteLine(Part1());
 Console.WriteLine(Part2());
 
 int Part1() => m[0].Sum(p =>
-    Enumerable.Range(1, 9).Aggregate(new[] { p }, (a, i) =>
-        a.SelectMany(q =>
-            m[i].GetNeighbors(q).Where(m[i].Contains)).Distinct().ToArray())
-    .Length);
+    Enumerable.Range(1, 9).Aggregate(new Grid(p), (a, i) =>
+        new(a.SelectMany(q =>
+            m[i].GetNeighbors(q).Where(m[i].Contains)))).Count);
 
 int Part2() =>
     Enumerable.Range(1, 9).Aggregate(m[0].ToArray(), (a, i) =>
