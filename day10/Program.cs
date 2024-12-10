fe408031575a21dbc2cfb5a64c7685b4f3362a74
Dmitry Shechtman <damnian@gmail.com>
2024-12-10 09:34:26 +0200

    Add MoveNext(predicate)

--- day10/Program.cs
+++ day10/Program.cs
@@ -9,8 +9,7 @@ Console.WriteLine(Part2());
 
 int Part1() => m[0].Sum(p =>
     Enumerable.Range(1, 9).Aggregate(new Grid(p), (a, i) =>
-        new(a.SelectMany(q =>
-            m[i].GetNeighbors(q).Where(m[i].Contains)))).Count);
+        a.MoveNext(m[i].Contains)).Count);
 
 int Part2() =>
     Enumerable.Range(1, 9).Aggregate(m[0].AsEnumerable(), (a, i) =>
