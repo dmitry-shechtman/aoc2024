07dfa9813288bd2e4a5c6d25a7481f32b359855a
Dmitry Shechtman <damnian@gmail.com>
2024-12-10 17:37:09 +0200

    Simplify sources

--- day10/Program.cs
+++ day10/Program.cs
@@ -2,16 +2,16 @@
 using aoc.Grids;
 
 var s = File.ReadAllText("input.txt").Trim();
-var m = MultiGrid.Parse(s, "0123456789");
+var m = MultiGrid.Parse(s, "0123456789").ToArray();
 
 Console.WriteLine(Part1());
 Console.WriteLine(Part2());
 
 int Part1() => m[0].Sum(p =>
-    Enumerable.Range(1, 9).Aggregate(new Grid(p), (a, i) =>
-        a.MoveNext(m[i].Contains)).Count);
+    m[1..10].Aggregate(new Grid(p), (a, g) =>
+        a.MoveNext(g.Contains)).Count);
 
 int Part2() =>
-    Enumerable.Range(1, 9).Aggregate(m[0].AsEnumerable(), (a, i) =>
-        a.SelectMany(q =>
-            m[i].GetNeighbors(q).Where(m[i].Contains))).Count();
+    m[1..10].Aggregate(m[0].AsEnumerable(), (a, g) =>
+        a.SelectMany(p =>
+            g.GetNeighbors(p).Where(g.Contains))).Count();
