c82c1b747a3098616b3d5167fa45efeb4f93fa92
Dmitry Shechtman <damnian@gmail.com>
2024-12-11 19:28:29 +0200

    Optimize day11

--- day11/Program.cs
+++ day11/Program.cs
@@ -2,24 +2,36 @@
 
 var input = File.ReadAllText("input.txt").Trim()
     .Split(' ')
-    .Select(long.Parse);
+    .Select(long.Parse)
+    .ToArray();
 
 Console.WriteLine(Solve(25));
 Console.WriteLine(Solve(75));
 
-long Solve(int n) =>
-    input.Sum(v => Calc(v, n));
-
-long Calc(long v, long d)
+long Solve(int n)
 {
-    if (d == 0)
+    long sum = 0;
+    for (int i = 0; i < input.Length; i++)
+        sum += Calc(input[i], n);
+    return sum;
+}
+
+long Calc(long value, long depth)
+{
+    if (depth == 0)
         return 1;
-    long k, p, m;
-    if (!cache.TryGetValue(k = v << 7 | --d, out long r))
-        cache.Add(k, r = v == 0
-            ? Calc(1, d)
-            : ((p = (long)Math.Log10(v) + 1) & 1) != 0
-                ? Calc(v * 2024, d)
-                : Calc(v / (m = (long)Math.Pow(10, p >> 1)), d) + Calc(v % m, d));
+    if (value == 0)
+        return Calc(1, depth - 1);
+    long key, div, mod;
+    if (!cache.TryGetValue(key = value << 7 | --depth, out long r))
+    {
+        for ((div, mod) = (10, 10); r == 0; (div, mod) = (div * 100, mod * 10))
+            r = value < div
+                ? Calc(value * 2024, depth)
+                : value < div * 10
+                    ? Calc(value / mod, depth) + Calc(value % mod, depth)
+                    : 0;
+        cache.Add(key, r);
+    }
     return r;
 }
