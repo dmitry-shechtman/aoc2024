39770be028ab544eeac53621ca3169410fa78e71
Dmitry Shechtman <damnian@gmail.com>
2024-12-16 02:33:37 +0200

    Optimize day15

--- day15/Program.cs
+++ day15/Program.cs
@@ -1,7 +1,4 @@
-﻿using aoc;
-using aoc.Grids;
-
-const int NONE  = 0;
+﻿const int NONE  = 0;
 const int LEFT  = 1;
 const int RIGHT = 2;
 const int BOX   = 3;
@@ -10,87 +7,113 @@ const int ROBOT = 5;
 
 var cc = ".[]O#@";
 
-Matrix Even  = (2, 0, 0, 1, 0, 0);
-Matrix Odd   = (2, 0, 0, 1, 1, 0);
-Vector Score = (1, 100);
-
 var s = File.ReadAllText("input.txt")
     .AsSpan()
     .Trim();
 
 var index = s.IndexOf("\n\n");
-var multi = MultiGrid.Parse(s[..index], cc, out var size);
-var path = Grid.ParseVectors(s[index..], '\n');
+var field = s[..index];
+var width = field.IndexOf('\n');
+var height = (field.Length + 1) / (width + 1);
+
+var grid0 = new int[width * height];
+for (int i = 0, j = 0; i < field.Length; i++)
+    if (field[i] != '\n')
+        grid0[j++] = cc.IndexOf(field[i]);
+
+int[] headings = new[] { -width, 1, width, -1 };
+List<int> path = new();
+for (int i = index + 2; i < s.Length; i++)
+    if (s[i] != '\n')
+        path.Add(headings["^>v<".IndexOf(s[i])]);
 
 int[] grid = Array.Empty<int>();
-Stack<(Vector, int)> stack = new();
-Queue<Vector> queue = new();
+int[] stack = new int[field.Length];
+int count;
 
-Console.WriteLine(Solve(Init1));
-Console.WriteLine(Solve(Init2));
-Console.WriteLine(GetString());
+Console.WriteLine(Solve(Init1, out _));
+Console.WriteLine(Solve(Init2, out var pos));
+Console.WriteLine(GetString(pos));
 
-int Solve(Func<Vector> init)
+int Solve(Func<int> init, out int pos)
 {
-    var pos = init();
-    SetValue(pos, ROBOT);
+    pos = init();
     foreach (var vec in path)
     {
-        stack.Clear();
-        if (!TryAdd(pos, vec))
+        count = 0;
+        if (!TryAdd(pos + vec, vec))
             continue;
-        foreach (var (box, _) in stack)
-            SetValue(box, NONE);
-        foreach (var (box, value) in stack)
-            SetValue(box + vec, value);
+        for (int i = 0; i < count; i++)
+            grid[stack[i] & 0xFFFF] = NONE;
+        for (int i = 0; i < count; i++)
+            grid[(stack[i] & 0xFFFF) + vec] = stack[i] >> 16;
         pos += vec;
     }
     return GetScore();
 }
 
-Vector Init1()
+int Init1()
 {
-    grid = new int[size.Length];
-    foreach (var box in multi[WALL])
-        SetValue(box, WALL);
-    foreach (var box in multi[BOX])
-        SetValue(box, BOX);
-    return multi[ROBOT].Single();
+    grid = new int[width * height];
+    var pos = 0;
+    for (int i = 0; i < grid.Length; i++)
+        if (grid0[i] == ROBOT)
+            pos = i;
+        else
+            grid[i] = grid0[i];
+    return pos;
 }
 
-Vector Init2()
+int Init2()
 {
-    size = new((Vector)size * Even);
-    grid = new int[size.Length];
-    foreach (var box in multi[WALL])
-        SetValue(box * Even, WALL);
-    foreach (var box in multi[WALL])
-        SetValue(box * Odd, WALL);
-    foreach (var box in multi[BOX])
-        SetValue(box * Even, LEFT);
-    foreach (var box in multi[BOX])
-        SetValue(box * Odd, RIGHT);
-    return multi[ROBOT].Single() * Even;
+    width *= 2;
+    grid = new int[width * height];
+    var pos = 0;
+    for (int i = 0; i < grid.Length; i++)
+        (grid[i], pos) = grid0[i >> 1] switch
+        {
+            BOX => ((i & 1) + 1, pos),
+            WALL => (WALL, pos),
+            ROBOT when (i & 1) == 0 => (NONE, i),
+            _ => (NONE, pos)
+        };
+    for (int i = 0; i < path.Count; i++)
+        path[i] = Math.Abs(path[i]) > 1 ? path[i] << 1 : path[i];
+    return pos;
 }
 
-bool TryAdd(Vector pos, Vector vec)
+bool TryAdd(int pos, int vec)
 {
-    var value = GetValue(pos);
-    return value == NONE
-        || (value != WALL
-        && TryAdd(pos + vec, vec)
-        && TryAddBox(pos, vec, value));
-}
-
-bool TryAddBox(Vector pos, Vector vec, int value)
-{
-    stack.Push((pos, value));
-    if (vec.x == 0 && value < BOX)
+    var value = grid[pos];
+    return value switch
     {
-        pos += (value == LEFT) ? (1, 0) : (-1, 0);
+        NONE => true,
+        BOX => TryAddBox(pos, vec, value),
+        LEFT => TryAddBox2(pos, vec, value),
+        RIGHT => TryAddBox2(pos, vec, value),
+        WALL => false,
+        _ => throw new()
+    };
+}
+
+bool TryAddBox(int pos, int vec, int value)
+{
+    if (!TryAdd(pos + vec, vec))
+        return false;
+    stack[count++] = pos | value << 16;
+    return true;
+}
+
+bool TryAddBox2(int pos, int vec, int value)
+{
+    if (!TryAddBox(pos, vec, value))
+        return false;
+    if (Math.Abs(vec) > 1)
+    {
+        pos += 3 - (value << 1);
         if (!TryAdd(pos + vec, vec))
             return false;
-        stack.Push((pos, value ^ BOX));
+        stack[count++] = pos | (value ^ BOX) << 16;
     }
     return true;
 }
@@ -98,24 +121,19 @@ bool TryAddBox(Vector pos, Vector vec, int value)
 int GetScore()
 {
     var total = 0;
-    for (int y = 0, i = 0; y < size.Height; y++)
-        for (int x = 0; x < size.Width; x++, i++)
-            if (grid[i] == LEFT || grid[i] == BOX)
-                total += Score.Dot((x, y));
+    for (int y = 0, i = 0, score = 0; y < height; ++y, score += (100 - width))
+        for (int x = 0; x < width; ++x, ++i, ++score)
+            if ((grid[i] & LEFT) != 0)
+                total += score;
     return total;
 }
 
-int GetValue(Vector pos) =>
-    grid[pos.y * size.Width + pos.x];
-
-void SetValue(Vector pos, int value) =>
-    grid[pos.y * size.Width + pos.x] = value;
-
-string GetString()
+string GetString(int pos)
 {
-    char[] chars = new char[(size.Width + 1) * size.Height];
-    for (int y = 0, i = 0, j = 0; y < size.Height; y++, chars[j++] = '\n')
-        for (int x = 0; x < size.Width; x++, i++)
+    grid[pos] = ROBOT;
+    char[] chars = new char[(width + 1) * height];
+    for (int y = 0, i = 0, j = 0; y < height; y++, chars[j++] = '\n')
+        for (int x = 0; x < width; x++, i++)
             chars[j++] = cc[grid[i]];
     return new string(chars);
 }
