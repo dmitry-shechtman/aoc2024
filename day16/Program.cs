8829be15e2618fcab86e390e9cf9e169470d8066
Dmitry Shechtman <damnian@gmail.com>
2024-12-17 00:16:57 +0200

    Optimize day16

--- day16/Program.cs
+++ day16/Program.cs
@@ -4,11 +4,18 @@ using aoc.Grids;
 const int Penalty = 1000;
 
 var input = File.ReadAllText("input.txt").Trim();
-var multi = MultiGrid.Parse(input, "#SE");
-var (walls, start, end) = (multi[0], multi[1].Single(), multi[2].Single());
+var multi = MultiGrid.Parse(input, "#SE", out var size);
 
-Dictionary<Vector, int> costs = new();
-Dictionary<Vector, PathValue> paths = new();
+var start = size.GetIndex(multi[1].Single());
+var end   = size.GetIndex(multi[2].Single());
+var walls = new bool[size.Length];
+foreach (var pos in multi[0])
+    size.SetValue(walls, pos, true);
+
+var costs = new int[size.Length];
+costs.Fill(int.MaxValue);
+var paths = new PathValue[size.Length];
+paths.Fill(new(int.MaxValue, new()));
 Queue<QueueItem> queue = new();
 
 Console.WriteLine(Part1());
@@ -16,26 +23,28 @@ Console.WriteLine(Part2());
 
 int Part1()
 {
-    Vector pos, vec, vecA, vecB;
-    int cost, cost1;
+    Vector vec, vecA, vecB;
+    int pos, offset, offsetA, offsetB, cost, cost1;
     queue.Enqueue(new(start, Vector.East, 0));
 
 
     while (queue.TryDequeue(out var item))
     {
-        vec = item.Vec;
+        (pos, vec, cost) = item;
         vecA = (vec.y, vec.x);
         vecB = -vecA;
+        offset = size.GetIndex(vec);
+        offsetA = size.GetIndex(vecA);
+        offsetB = size.GetIndex(vecB);
 
-
-        for (pos = item.Pos, cost = item.Cost, cost1 = cost + Penalty + 1;
-            !walls.Contains(pos) && pos != end;
-            pos += vec, ++cost, ++cost1)
+        for (cost1 = cost + Penalty + 1;
+            !walls[pos] && pos != end;
+            pos += offset, ++cost, ++cost1)
         {
-            if (TryAdd(pos + vecA, cost1))
-                queue.Enqueue(new(pos + vecA, vecA, cost1));
-            if (TryAdd(pos + vecB, cost1))
-                queue.Enqueue(new(pos + vecB, vecB, cost1));
+            if (TryAdd(pos + offsetA, cost1))
+                queue.Enqueue(new(pos + offsetA, vecA, cost1));
+            if (TryAdd(pos + offsetB, cost1))
+                queue.Enqueue(new(pos + offsetB, vecB, cost1));
 
         }
 
@@ -45,9 +54,9 @@ int Part1()
     return costs[end];
 }
 
-bool TryAdd(Vector pos, int cost)
+bool TryAdd(int pos, int cost)
 {
-    if (costs.TryGetValue(pos, out var cost2) && cost2 <= cost)
+    if (costs[pos] <= cost)
         return false;
     costs[pos] = cost;
     return true;
@@ -55,21 +64,23 @@ bool TryAdd(Vector pos, int cost)
 
 int Part2()
 {
+    Vector vec, vecA, vecB;
+    int pos, offset, cost, cost2;
     queue.Enqueue(new(start, Vector.East, 0));
-    Vector pos, vec, vecA, vecB;
-    int cost, cost2;
-    List<Vector> path = new();
+    List<int> path = new();
 
     while (queue.TryDequeue(out var item))
     {
-        vec = item.Vec;
+        (pos, vec, cost) = item;
         vecA = (vec.y, vec.x);
         vecB = -vecA;
+        offset = size.GetIndex(vec);
+
         path.Clear();
 
-        for (pos = item.Pos, cost = item.Cost, cost2 = cost + Penalty;
-            !walls.Contains(pos) && pos != end;
-            pos += vec, ++cost, ++cost2)
+        for (cost2 = cost + Penalty;
+            !walls[pos] && pos != end;
+            pos += offset, ++cost, ++cost2)
         {
             if (TryAdd2(pos, cost2, path))
                 queue.Enqueue(new(pos, vecA, cost2));
@@ -84,14 +95,15 @@ int Part2()
     return CountPathItems();
 }
 
-bool TryAdd2(Vector pos, int cost, List<Vector> path)
+bool TryAdd2(int pos, int cost, List<int> path)
 {
-    if (paths.TryGetValue(pos, out var value) && value.Cost <= cost)
+    var (cost2, path2) = paths[pos];
+    if (cost2 <= cost)
     {
-        if (value.Cost < cost)
+        if (cost2 < cost)
             return false;
         for (int i = 1; i < path.Count; i++)
-            value.Path.Add(path[i]);
+            path2.Add(path[i]);
         return true;
     }
     paths[pos] = new(cost, new(path));
@@ -100,15 +112,14 @@ bool TryAdd2(Vector pos, int cost, List<Vector> path)
 
 int CountPathItems()
 {
-    HashSet<Vector> set = new() { end };
-    Queue<Vector> queue2 = new(set);
+    HashSet<int> set = new() { end };
+    Queue<int> queue2 = new(set);
     while (queue2.TryDequeue(out var item))
-        if (paths.TryGetValue(item, out var path2))
-            foreach (var pos2 in path2.Path)
-                if (set.Add(pos2))
-                    queue2.Enqueue(pos2);
+        foreach (var pos2 in paths[item].Path)
+            if (set.Add(pos2))
+                queue2.Enqueue(pos2);
     return set.Count;
 }
 
-record struct QueueItem(Vector Pos, Vector Vec = default, int Cost = 0);
-record struct PathValue(int Cost, HashSet<Vector> Path);
+record struct QueueItem(int Pos, Vector Vec, int Cost);
+record struct PathValue(int Cost, HashSet<int> Path);
