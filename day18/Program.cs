34d6edc5970af275dce983d5d6abe96afcca8077
Dmitry Shechtman <damnian@gmail.com>
2024-12-18 12:37:29 +0200

    Simplify day18

--- day18/Program.cs
+++ day18/Program.cs
@@ -7,10 +7,7 @@ var points = input.Split('\n')
     .ToArray();
 
 VectorRange range = points.Range();
-Size size = new(range);
-
-HashSet<Vector> walls = new(points[..1024]);
-Dictionary<Vector, int> costs = new();
+HashSet<Vector> balls = new();
 Queue<QueueItem> queue = new();
 Vector[] headings = Grid.Headings;
 
@@ -18,42 +15,37 @@ Console.WriteLine(Part1());
 Console.WriteLine(Part2());
 
 int Part1() =>
-    TryFindShortestPath(out var value) ? value : 0;
+    TryFindShortestPath(1024, out var value) ? value : 0;
 
 Vector Part2() =>
-    FindBlock(walls.Count, points.Length);
+    FindBlock(1024, points.Length);
 
 Vector FindBlock(int start, int end)
 {
     var pivot = (start + end) / 2;
-    walls.Clear();
-    points[..pivot].All(walls.Add);
-    var found = TryFindShortestPath(out _);
+    var found = TryFindShortestPath(pivot, out _);
     return end - start == 1
         ? points[found ? pivot : pivot - 1]
         : FindBlock(found ? pivot : start, found ? end : pivot);
 }
 
-bool TryFindShortestPath(out int cost)
+bool TryFindShortestPath(int index, out int dist)
 {
     Vector pos;
-    costs.Clear();
+    balls.Clear();
+    points[..index].All(balls.Add);
+    range.Border(-1).All(balls.Add);
     queue.Clear();
     queue.Enqueue(default);
-    while (queue.TryDequeue(out var item))
-        foreach (var vec in headings)
-            if (vec != item.Vec && size.Contains(pos = item.Pos + vec) &&
-                !walls.Contains(pos) && TryAdd(pos, item.Cost + 1))
-                    queue.Enqueue(new(pos, -vec, item.Cost + 1));
-    return costs.TryGetValue(range.Max, out cost);
+    while (queue.TryDequeue(out var item) && (dist = ++item.Dist) > 0)
+        foreach (var vec in Grid.Headings)
+            if (vec != item.Vec && balls.Add(pos = item.Pos + vec))
+                if (pos == range.Max)
+                    return true;
+                else
+                    queue.Enqueue(new(pos, -vec, dist));
+    dist = 0;
+    return false;
 }
 
-bool TryAdd(Vector pos, int cost)
-{
-    if (costs.TryGetValue(pos, out var cost2) && cost2 <= cost)
-        return false;
-    costs[pos] = cost;
-    return true;
-}
-
-record struct QueueItem(Vector Pos, Vector Vec, int Cost);
+record struct QueueItem(Vector Pos, Vector Vec, int Dist);
