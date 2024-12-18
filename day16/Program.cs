584850fb0287f478c6f3d4302a10afcd8735b190
Dmitry Shechtman <damnian@gmail.com>
2024-12-18 21:27:23 +0200

    Rename cost to dist

--- day16/Program.cs
+++ day16/Program.cs
@@ -13,8 +13,8 @@ foreach (var pos in multi[0])
     size.SetValue(walls, pos, true);
 var width1 = size.width + 1;
 
-var costs = new int[size.Length];
-costs.Fill(int.MaxValue);
+var dists = new int[size.Length];
+dists.Fill(int.MaxValue);
 var paths = new PathValue[size.Length];
 paths.Fill(new(int.MaxValue, new()));
 Queue<QueueItem> queue = new();
@@ -25,85 +25,85 @@ Console.WriteLine(Part2());
 int Part1()
 {
     int vec, vecA, vecB;
-    int pos, cost, cost1;
+    int pos, dist, dist1;
     queue.Enqueue(new(start, 1, 0));
 
 
     while (queue.TryDequeue(out var item))
     {
-        (pos, vec, cost) = item;
+        (pos, vec, dist) = item;
         vecA = vec < 0 ? width1 + vec : width1 - vec;
         vecB = -vecA;
 
-        for (cost1 = cost + Penalty + 1;
+        for (dist1 = dist + Penalty + 1;
             !walls[pos] && pos != end;
-            pos += vec, ++cost, ++cost1)
+            pos += vec, ++dist, ++dist1)
         {
-            if (TryAdd(pos + vecA, cost1))
-                queue.Enqueue(new(pos + vecA, vecA, cost1));
+            if (TryAdd(pos + vecA, dist1))
+                queue.Enqueue(new(pos + vecA, vecA, dist1));
 
-            if (TryAdd(pos + vecB, cost1))
-                queue.Enqueue(new(pos + vecB, vecB, cost1));
+            if (TryAdd(pos + vecB, dist1))
+                queue.Enqueue(new(pos + vecB, vecB, dist1));
 
         }
 
-        TryAdd(pos, cost);
+        TryAdd(pos, dist);
     }
 
-    return costs[end];
+    return dists[end];
 }
 
-bool TryAdd(int pos, int cost)
+bool TryAdd(int pos, int dist)
 {
-    if (costs[pos] <= cost)
+    if (dists[pos] <= dist)
         return false;
-    costs[pos] = cost;
+    dists[pos] = dist;
     return true;
 }
 
 int Part2()
 {
     int vec, vecA, vecB;
-    int pos, cost, cost2;
+    int pos, dist, dist2;
     queue.Enqueue(new(start, 1, 0));
     List<int> path = new();
 
     while (queue.TryDequeue(out var item))
     {
-        (pos, vec, cost) = item;
+        (pos, vec, dist) = item;
         vecA = vec < 0 ? width1 + vec : width1 - vec;
         vecB = -vecA;
         path.Clear();
-        for (cost2 = cost + Penalty;
+        for (dist2 = dist + Penalty;
             !walls[pos] && pos != end;
-            pos += vec, ++cost, ++cost2)
+            pos += vec, ++dist, ++dist2)
         {
-            if (TryAdd2(pos, cost2, path))
+            if (TryAdd2(pos, dist2, path))
             {
-                queue.Enqueue(new(pos, vecA, cost2));
-                queue.Enqueue(new(pos, vecB, cost2));
+                queue.Enqueue(new(pos, vecA, dist2));
+                queue.Enqueue(new(pos, vecB, dist2));
             }
             path.Add(pos);
         }
 
-        TryAdd2(pos, cost, path);
+        TryAdd2(pos, dist, path);
     }
 
     return CountPathItems();
 }
 
-bool TryAdd2(int pos, int cost, List<int> path)
+bool TryAdd2(int pos, int dist, List<int> path)
 {
-    var (cost2, path2) = paths[pos];
-    if (cost2 <= cost)
+    var (dist2, path2) = paths[pos];
+    if (dist2 <= dist)
     {
-        if (cost2 < cost)
+        if (dist2 < dist)
             return false;
         for (int i = 1; i < path.Count; i++)
             path2.Add(path[i]);
         return true;
     }
-    paths[pos] = new(cost, new(path));
+    paths[pos] = new(dist, new(path));
     return true;
 }
 
@@ -118,5 +118,5 @@ int CountPathItems()
     return set.Count;
 }
 
-record struct QueueItem(int Pos, int Vec, int Cost);
-record struct PathValue(int Cost, HashSet<int> Path);
+record struct QueueItem(int Pos, int Vec, int Dist);
+record struct PathValue(int Dist, HashSet<int> Path);
