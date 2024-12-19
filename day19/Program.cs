a723b7090de7b08cf86efc666de096ef4b426001
Dmitry Shechtman <damnian@gmail.com>
2024-12-19 13:16:05 +0200

    Fix Count()

--- day19/Program.cs
+++ day19/Program.cs
@@ -15,8 +15,7 @@ int Part1() =>
 long Part2() =>
     patterns.Sum(s => counts[s]);
 
-long Count(string s) => counts.TryGetValue(s, out var count)
-    ? count
-    : counts[s] = (towels.Contains(s) ? 1 : 0) +
+long Count(string s) => counts.GetOrAdd(s,
+    s => (towels.Contains(s) ? 1 : 0) +
         towels.Where(t => t.Length < s.Length && s[..t.Length] == t)
-            .Sum(t => Count(s[t.Length..]));
+            .Sum(t => Count(s[t.Length..])));
