24d62ee68d271c222cee14ab24f51f98fea5c279
Dmitry Shechtman <damnian@gmail.com>
2024-12-22 13:13:37 +0200

    Optimize Max()

--- day22/Program.cs
+++ day22/Program.cs
@@ -1,11 +1,12 @@
-﻿using aoc;
-using System.Collections.Concurrent;
+﻿using System.Collections;
 
 var input = File.ReadAllText("input.txt").Trim().Split('\n')
     .Select(long.Parse)
     .ToArray();
 
-ConcurrentDictionary<Vector4D, int> sums = new();
+const int MAGIC = 130321;
+int[] sums = new int[MAGIC];
+BitArray seen = new(MAGIC);
 
 Console.WriteLine(Part1());
 Console.WriteLine(Part2());
@@ -14,27 +15,28 @@ long Part1() =>
     input.Sum(x => Enumerable.Range(0, 2000).Aggregate(x, MoveNext));
 
 long Part2() =>
-    input.AsParallel().Max(Max);
+    input.Aggregate(0, Max);
 
 long MoveNext(long secret, int _ = 0)
 {
-    secret = (secret ^ (secret << 6)) & 0xFFFFFF;
-    secret = (secret ^ (secret >> 5));
-    secret = (secret ^ (secret << 11)) & 0xFFFFFF;
+    secret ^= secret << 6;
+    secret &= 0xFFFFFF;
+    secret ^= secret >> 5;
+    secret ^= secret << 11;
+    secret &= 0xFFFFFF;
     return secret;
 }
 
-int Max(long sec)
+int Max(int max, long sec)
 {
-    int max = 0;
-    HashSet<Vector4D> seen = new();
-    Vector4D key = default;
+    seen.SetAll(false);
+    int key = default;
     for (var (prev, curr, i) = (0, 0, 0); i <= 2000; sec = MoveNext(sec), ++i)
     {
         (prev, curr) = (curr, (int)(sec % 10));
-        key = (key.y, key.z, key.w, curr - prev);
-        if (i >= 4 && seen.Add(key))
-            max = Math.Max(max, sums.AddOrUpdate(key, curr, (_, sum) => sum + curr));
+        key = (key * 19 + (curr - prev) + 9) % MAGIC;
+        if (i >= 4 && !seen[key] && (seen[key] = true))
+            max = Math.Max(max, sums[key] += curr);
     }
     return max;
 }
