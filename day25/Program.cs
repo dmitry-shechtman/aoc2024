0edbd781543acf52a6134a61bd6753c0d7a39fc8
Dmitry Shechtman <damnian@gmail.com>
2024-12-25 10:18:33 +0200

    Cleaner day25

--- day25/Program.cs
+++ day25/Program.cs
@@ -1,12 +1,12 @@
-﻿var s = File.ReadAllText("input.txt").Trim();
+﻿var s = File.OpenText("input.txt");
 var v = new List<int[]>[] { new(), new() };
 
-for (int i = 0, k, x, y; i < s.Length; ++i)
-    for (y = 0, k = s[i] & 1, v[k].Add(new int[5]); y < 7; ++y, ++i)
+for (int k, x, y; s.Peek() >= 0; s.Read())
+    for (y = 0, k = s.Peek() & 1, v[k].Add(new int[5]); y < 7; ++y, s.Read())
         for (x = 0; x < 5; ++x)
-            v[k][^1][x] += s[i++] & 1;
+            v[k][^1][x] += s.Read() & 1;
 
 Console.WriteLine(Part1());
 
 int Part1() =>
-    v[0].Sum(a => v[1].Count(b => a.Zip(b, (a, b) => a + b <= 7).All(_ => _)));
+    v[0].Sum(a => v[1].Count(b => a.Zip(b, (a, b) => a + b < 8).All(_ => _)));
