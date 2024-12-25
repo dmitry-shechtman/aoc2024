886c35efaec026560f2a99d00bddc13e59d67762
Dmitry Shechtman <damnian@gmail.com>
2024-12-25 02:00:55 +0200

    Simplify Part1()

--- day24/Program.cs
+++ day24/Program.cs
@@ -6,36 +6,24 @@ var input = File.ReadAllText("input.txt").Trim();
 var vals = regex.GetAllValues(input);
 
 var gates = vals["c"].Select((k, i) =>
-        (k, v: new Gate(ParseOp(vals["op"][i]), vals["a"][i], vals["b"][i])))
+        (k, v: (vals["op"][i], vals["a"][i], vals["b"][i])))
     .Concat(vals["k"].Select((k, i) =>
-        (k, v: new Gate((Op)(vals["v"][i][0] - '0'), "", ""))))
+        (k, v: (vals["v"][i], "", ""))))
     .ToDictionary(t => t.k, t => t.v);
 
 Console.WriteLine(Part1());
 
 long Part1() =>
-    Enumerable.Range(0, 64).Reverse().Select(i => $"z{i:d02}")
-        .Where(gates.ContainsKey)
+    gates.Keys.Where(t => t[0] == 'z')
+        .OrderDescending()
         .Aggregate(0L, (a, k) => a << 1 | GetValue(k));
 
 long GetValue(string key) => gates[key] switch
 {
-    (Op.NIL, _, _) => 0,
-    (Op.ONE, _, _) => 1,
-    (Op.AND, var a, var b) => GetValue(a) & GetValue(b),
-    (Op.OR,  var a, var b) => GetValue(a) | GetValue(b),
-    (Op.XOR, var a, var b) => GetValue(a) ^ GetValue(b),
+    ("0", _, _) => 0,
+    ("1", _, _) => 1,
+    ("AND", var a, var b) => GetValue(a) & GetValue(b),
+    ("OR",  var a, var b) => GetValue(a) | GetValue(b),
+    ("XOR", var a, var b) => GetValue(a) ^ GetValue(b),
     _ => throw new()
 };
-
-Op ParseOp(string op) => op switch
-{
-    "AND" => Op.AND,
-    "OR"  => Op.OR,
-    "XOR" => Op.XOR,
-    _ => throw new NotImplementedException()
-};
-
-enum Op { NIL, ONE, AND, OR, XOR }
-
-record struct Gate(Op Op, string A, string B);
