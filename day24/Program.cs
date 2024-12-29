0c1681be60bd29a635f09ba50ab390b390f62fe0
Dmitry Shechtman <damnian@gmail.com>
2024-12-29 22:09:40 +0200

    Add day24 part 2

--- day24/Program.cs
+++ day24/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 
+using ANF = System.Collections.Generic.SortedSet<System.UInt128>;
+
 const int COUNT = 64;
 const int Z00 = COUNT * 2;
 
@@ -8,17 +10,30 @@ Regex regex = new(@"^((?<k>[xy]\d\d): (?<v>0|1)\n)+(\n(?<a>[a-w]{3}|[xy]\d\d) (?
 var input = File.ReadAllText("input.txt").Trim();
 var vals = regex.GetAllValues(input);
 
-var gates = BuildCircuit(out var values);
+var gates = BuildCircuit(out var keys, out var values);
+
+ANF[] anfs, exps;
+SortedSet<string> swap = new();
 
 Console.WriteLine(Part1());
+Console.WriteLine(Part2());
 
 long Part1() =>
     Enumerable.Range(Z00, COUNT)
         .Aggregate(0L, (a, i) => a | GetValue(i) << i);
 
-Gate[] BuildCircuit(out int[] values)
+string Part2()
 {
-    (var keys, values) = BuildIndex();
+    BuildExpected();
+    do
+        BuildActual();
+    while (TrySwapAny());
+    return string.Join(',', swap);
+}
+
+Gate[] BuildCircuit(out string[] keys, out int[] values)
+{
+    (keys, values) = BuildIndex();
 
     var gates = new Gate[keys.Length];
 
@@ -34,9 +49,12 @@ Gate[] BuildCircuit(out int[] values)
 (string[] keys, int[] values) BuildIndex()
 {
     List<string> keys = new();
-    foreach (var c in "xyz")
-        for (int i = 0; i < COUNT; i++)
+    for (int i = 0; i < COUNT; i++)
+        foreach (var c in "xy")
             keys.Add($"{c}{i:d02}");
+
+    for (int i = 0; i < COUNT; i++)
+        keys.Add($"z{i:d02}");
 
     foreach (var key in vals["c"])
         if (key[0] != 'z')
@@ -70,6 +88,119 @@ Op ParseOp(string op) => op switch
     "XOR" => Op.XOR,
     _ => throw new NotImplementedException()
 };
+
+// Build ANFs for adder outputs 
+void BuildExpected()
+{
+    exps = new ANF[COUNT];
+
+    for (var (i, m) = (Z00, UInt128.One); gates[i] != default; i++, m <<= 2)
+    {
+        ANF exp = new() { m, m << 1 };
+        if (gates[i + 1] == default) // Last bit
+            exp.Clear(); // No x_i or y_i
+        if (i > Z00) // Skip bit 0
+            exp.Add(m >> 1 | m >> 2); // Carry
+        exps[i - Z00] = exp;
+    }
+}
+
+// Build ANFs for actual circuit
+void BuildActual()
+{
+    anfs = new ANF[gates.Length];
+
+    // Initialize input variables
+    for (var (i, m) = (0, UInt128.One); i < Z00; i++, m <<= 1)
+        anfs[i] = new() { m }; // x_i maps to 2^2i
+
+    // Add gates to the array
+    for (int i = Z00; i < gates.Length; i++)
+        anfs[i] = gates[i] == default ? new() : GetANF(i);
+}
+
+// Retrieve an ANF or create one if not found
+ANF GetANF(int i) =>
+    anfs[i] ?? (anfs[i] = DoGetANF(i));
+
+// Recursively build an ANF
+ANF DoGetANF(int i)
+{
+    var (Op, A, B) = gates[i];
+    var a = GetANF(A);
+    var b = GetANF(B);
+    return Op switch
+    {
+        Op.AND => Multiply(a, b),
+        Op.OR  => Add(Add(a, b), Multiply(a, b)),
+        Op.XOR => Add(a, b),
+        _ => throw new NotImplementedException(),
+    };
+}
+
+// Multiply two ANFs (AND operation)
+ANF Multiply(ANF left, ANF right)
+{
+    ANF result = new();
+    foreach (var key1 in left)
+        foreach (var key2 in right)
+            if (!result.Remove(key1 ^ key2))
+                result.Add(key1 ^ key2);
+    return result;
+}
+
+// Add two ANFs (XOR operation)
+ANF Add(ANF left, ANF right)
+{
+    ANF result = new(left);
+    foreach (var key in right)
+        if (!result.Remove(key))
+            result.Add(key);
+    return result;
+}
+
+// Look for an invalid output; try swapping if found
+bool TrySwapAny()
+{
+    for (int i = Z00; anfs[i].Count > 0; i++)
+    {
+        var exp = exps[i - Z00];
+        if (!exp.SequenceEqual(anfs[i]))
+            return TrySwap(exp, i);
+    }
+    return false;
+}
+
+// Recursively try swapping
+bool TrySwap(ANF anf, int i)
+{
+    if (DoTrySwap(anf, i))
+        return true;
+    var (op, a, b) = gates[i];
+    return op == Op.XOR &&
+        (TrySwap(Add(anf, anfs[a]), b) ||
+        TrySwap(Add(anf, anfs[b]), a));
+}
+
+// Search an equivalent ANF; swap if found
+bool DoTrySwap(ANF anf, int i)
+{
+    if (!TryFind(anf, out var j))
+        return false;
+    (gates[i], gates[j]) = (gates[j], gates[i]);
+    swap.Add(keys[i]);
+    swap.Add(keys[j]);
+    return true;
+}
+
+// Search an equivalent ANF
+bool TryFind(ANF anf, out int i)
+{
+    for (i = Z00; i < gates.Length; i++)
+        if (anf.SequenceEqual(anfs[i]))
+            return true;
+    return false;
+}
 
 enum Op { NIL, ONE, AND, OR, XOR }
 
