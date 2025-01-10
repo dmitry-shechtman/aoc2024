210ef44b582ae13204c584f8f61e362a09a4cec0
Dmitry Shechtman <damnian@gmail.com>
2025-01-10 22:19:40 +0200

    Use DictionaryExtensions

--- day11/Program.cs
+++ day11/Program.cs
@@ -14,12 +14,11 @@ long Calc(long v, long d)
 {
     if (d == 0)
         return 1;
-    long k, p, m;
-    if (!cache.TryGetValue(k = v << 7 | --d, out long r))
-        cache.Add(k, r = v == 0
+    long p, m;
+    return cache.GetOrAdd(v << 7 | --d, _ =>
+        v == 0
             ? Calc(1, d)
             : ((p = (long)Math.Log10(v) + 1) & 1) != 0
                 ? Calc(v * 2024, d)
                 : Calc(v / (m = (long)Math.Pow(10, p >> 1)), d) + Calc(v % m, d));
-    return r;
 }
