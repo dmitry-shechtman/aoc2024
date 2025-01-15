149bbbd1e461873fa3800c92a6cd4d81f4126283
Dmitry Shechtman <damnian@gmail.com>
2025-01-15 15:06:10 +0200

    Extract INextBuilder

--- day10/Program.cs
+++ day10/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine(Part2());
 
 int Part1() => m[0].Sum(p =>
     m[1..10].Aggregate(new Grid(p), (a, g) =>
-        a.MoveNext(g.Contains)).Count);
+        Grid.Next.Move(a, g.Contains)).Count);
 
 int Part2() =>
     m[1..10].Aggregate(m[0].AsEnumerable(), (a, g) =>
